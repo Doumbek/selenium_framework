616109347a382fd0da56e72fdaffca26c1af1d14
Vitaliy Yakushenko <Vitaliy_Yakushenko@epam.com>
2015-08-17 18:36:16 +0300

    Add HelloWorldTest

---
+++ src/test/java/com/gmail/tests/EmptyTest.java
@@ -0,0 +1,14 @@
+package com.gmail.tests;
+
+import org.testng.annotations.Test;
+
+public class EmptyTest {
+
+	@Test
+	public void checkTest() {
+
+		System.out.println("Hello world!");
+
+	}
+
+}
